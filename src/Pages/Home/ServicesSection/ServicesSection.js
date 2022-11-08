b106d780cfa91888b715ba937f0a7531195b4f6b
FreelancerShijan <freelancershijan@gmail.com>
2022-11-08 16:30:03 +0600

    react-photo-view added

--- src/Pages/Home/ServicesSection/ServicesSection.js
+++ src/Pages/Home/ServicesSection/ServicesSection.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const ServicesSection = () => {
     const [services, setServices] = useState([]);
@@ -13,29 +15,33 @@ const ServicesSection = () => {
     console.log(services)
     return (
 
-        <div className='mx-5 grid md:grid-cols-3 gap-5 md:mx-auto mt-8 lg:mt-16'>
-            {
-                services.slice(0, 3).map(service => <div class="rounded-lg shadow-lg bg-white max-w-sm">
-                    <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                        <img class="rounded-t-lg" src={service?.photo} alt="" />
-                    </a>
-                    <div class="p-6">
-                        <h5 class="text-gray-900 text-xl font-medium mb-2">{service?.name}</h5>
-                        <p class="text-gray-700 text-base mb-4">
-                            {service.description.slice(0, 200)}
-                        </p>
-                        <div className='flex justify-between items-center'>
-                            <Link to='/services'>
-                                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Get This Service</button>
-                            </Link>
-                            <div>
-                                <h1 className='text-3xl font-semibold'>${service?.price}<span className='text-lg'>/mo</span></h1>
+        <PhotoProvider>
+            <div className='mx-5 grid md:grid-cols-3 gap-5 md:mx-auto mt-8 lg:mt-16'>
+                {
+                    services.slice(0, 3).map(service => <div class="rounded-lg shadow-lg bg-white max-w-sm">
+
+                        <PhotoView>
+                            <img class="rounded-t-lg" src={service?.photo} alt="" />
+                        </PhotoView>
+
+                        <div class="p-6">
+                            <h5 class="text-gray-900 text-xl font-medium mb-2">{service?.name}</h5>
+                            <p class="text-gray-700 text-base mb-4">
+                                {service.description.slice(0, 200)}
+                            </p>
+                            <div className='flex justify-between items-center'>
+                                <Link to='/services'>
+                                    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Get This Service</button>
+                                </Link>
+                                <div>
+                                    <h1 className='text-3xl font-semibold'>${service?.price}<span className='text-lg'>/mo</span></h1>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>)
-            }
-        </div>
+                    </div>)
+                }
+            </div>
+        </PhotoProvider>
 
     );
 };
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import AuthProvider from './utilities/AuthProvider/AuthProvider';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
