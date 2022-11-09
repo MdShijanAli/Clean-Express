c4c0fe45f799f97a6c18a9814116a8d0b0aab191
FreelancerShijan <freelancershijan@gmail.com>
2022-11-10 00:22:22 +0600

    review form privet

--- src/Pages/Services/ServiceDetails/SingleServiceDetails/ReviewSection/ReviewSection.js
+++ src/Pages/Services/ServiceDetails/SingleServiceDetails/ReviewSection/ReviewSection.js
@@ -50,6 +50,7 @@ const ReviewSection = ({ singleService }) => {
 
 
     return (
+
         <form onSubmit={AddUser}>
 
 
--- src/Pages/Services/ServiceDetails/SingleServiceDetails/SingleServiceDetails.js
+++ src/Pages/Services/ServiceDetails/SingleServiceDetails/SingleServiceDetails.js
@@ -1,14 +1,19 @@
 import { CheckIcon } from '@heroicons/react/24/solid';
-import React from 'react';
+import buttonOutlined from '@material-tailwind/react/theme/components/button/buttonOutlined';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../../utilities/AuthProvider/AuthProvider';
 import AllReviews from './ReviewSection/AllReviews/AllReviews';
 import ReviewSection from './ReviewSection/ReviewSection';
+import './SingleServiceDetails.css'
 
 const SingleServiceDetails = ({ singleService }) => {
+    const { user } = useContext(AuthContext);
     console.log(singleService);
     const { photo, description, descriptionPoint, name } = singleService;
     return (
         <div className='md:px-20 md:my-20'>
-            <img className='rounded-l-3xl w-full' src={photo} alt="" />
+            <img className='rounded-l-3xl w-full height' src={photo} alt="" />
             <h1 className='text-5xl my-5 font-semibold'>{name}</h1>
             <p className='mb-10 text-gray-600 font-semibold'>{description}</p>
             {
@@ -31,7 +36,12 @@ const SingleServiceDetails = ({ singleService }) => {
 
 
             <div>
-                <ReviewSection singleService={singleService}></ReviewSection>
+                {
+                    user?.uid ? <ReviewSection singleService={singleService}></ReviewSection> :
+                        <Link to='/login'>
+                            <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Please login to add a review</button>
+                        </Link>
+                }
             </div>
         </div >
     );
